10e59b5b64f2f89177a838efa3baeea7e6cacaee
Tim Murtaugh <tim@monkeydo.biz>
2013-02-19 10:59:41 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,113 @@
+<!doctype html>
+
+<html lang="en">
+
+<head>
+
+	<meta charset="utf-8">
+	
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	
+	<title>Has your Twitter account been hacked? What to do when Twitter starts tweeting without you.</title>
+	
+	<meta name="title" content="Has your Twitter account been hacked?">
+	<meta name="description" content="What to do when Twitter starts tweeting without you." />
+	
+	<meta name="viewport" content="width=device-width">
+	
+	<link rel="stylesheet" href="_/css/style.css">
+
+</head>
+
+<body>
+
+<div class="wrapper">
+
+	<header>
+	
+		<h1><a href="/">Has your Twitter account been hacked?</a></h1>
+	
+	</header>
+	
+	<article class="intro">
+	
+		<p>Is your Twitter account tweeting without you? There could be a few reasons, some easy to fix, some not so much but we'll persevere.</p>
+	
+	</article>
+	
+	<article>
+	
+		<h1>Reason 1: Rude or Rogue Applications <small>(How concerned should you be? Meh.)</small></h1>
+		
+		<p>The most likely culprit is an application or web site you've allowed to access your Twitter account. This may be a service like Disqus, which requires authentication to comment on blogs, or an app like Words With Friends which wants to make it as easy as possible for you to tell everyone how awesome Words With Friends is.</p>
+
+		<p>In each case, protocol dictates that they be very clear about when they will be posting a tweet to your account on your behalf, but not all developers are as upstanding as we would hope. Sometimes, either accidentally or maliciously, these apps may tweet using your account, to push their own agenda.</p>
+
+		<h2>The Solution?</h2>
+
+		<p>Super simple: Go to <a href="https://twitter.com/settings/applications">Twitter &gt; Settings &gt; Applications</a> and revoke access to the rude app in question. (Don't be afraid to partake in a little Twitter-shaming — those who abuse the public forum should be chastised in it as well.) While you're there, take a look at the full list and make sure the apps with access are ones you approve of (and remember giving access to).</p>
+		
+		<p>Be vicious — if you can't remember why an app has access to your account, revoke it. You can always restore it later.</p>
+	
+	</article>
+	
+	<article>
+	
+		<h1>Reason 2: Your Password has been Compromised <small>(How concerned should you be? Very.)</small></h1>
+		
+		<p>If you've been unable to identify which app the rogue tweets are coming from — or if you've identified the app, but have no memory of giving it access — it's possible you've been compromised at a much more significant level.</p>
+
+		<p>Someone, or something, may have stolen or figured out your Twitter password, and is using it to access your account directly.</p>
+
+		<h2>The Solution?</h2>
+		
+		<p>Obviously your first instinct is to immediately change your password — but let's wait for just one moment.</p>
+
+		<p>You have to ask yourself: How did they get your password? Is it possible your computer has been compromised? What about your email address?</p>
+
+		<p>Since at this point it's very likely you've been compromised at a serious level, it's time for a more comprehensive plan of attack:</p>
+
+		<p>(FYI, this is a modified version of <a href="http://twitter.com/kissane">Erin Kissane's</a> personal checklist, which can be found here: <a href="http://pastebin.com/Ukh7mpaR">http://pastebin.com/Ukh7mpaR</a>)</p>
+
+		<ol>
+			<li>Using a clean computer, lock down the email account associated with your Twitter account (change the password, review all security settings). If you use Gmail, consider turning on <a href="http://support.google.com/accounts/bin/answer.py?hl=en&answer=180744">two-step verification</a>.</li>
+			<li>Revoke all <a href="https://twitter.com/settings/applications">app permissions on Twitter</a> (you can restore the ones you need after completing this list).</li>
+			<li>Change your password, and take the opportunity to make it stronger. (Wondering how best to go about that? <a href="http://xkcd.com/936/">This XKCD comic</a> is for you.)</li>
+			<li>Do you use the same password for any other apps or web sites? (Tsk, tsk.) Change the passwords on any other services that use the same login credentials (and then consider using something like <a href="https://agilebits.com/onepassword">1Password</a> to help manage all the different passwords you should be creating).</li>
+			<li>Clear your browser of all cookies and cached data.</li>
+			<li>Scan your computers for adware/malware/viruses/etc.</li>
+			<li>Ask yourself if it's a good time to audit your other back up processes as well.</li>
+			<li>Have a richly deserved drink.</li>
+		</ol>
+	
+	</article>
+	
+	<footer>
+		
+		<p>
+			<a href="http://monkeydo.biz"><img src="_/img/md-logo.png" alt="" /></a>
+			<small>
+				Brought to you by your pals at <a href="http://monkeydo.biz" data-twitter="monkeydobiz">Monkey Do</a>.
+			</small>
+		</p>
+		
+	</footer>
+
+</div>
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-615784-33']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
+</body>
+</html>
